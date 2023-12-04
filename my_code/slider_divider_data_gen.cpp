5726bfdee812cb191bbbaf9678f4acab5fbe8213
Jonathan Ting <jonathan.ting07@gmail.com>
2023-12-04 15:24:58 -0600

    Comment slider_divider_data_gen.cpp

--- my_code/slider_divider_data_gen.cpp
+++ my_code/slider_divider_data_gen.cpp
@@ -10,59 +10,76 @@
 
 using namespace std;
 
-int main() {
-    // For each file in the sliders folder, print the file name
+int main()
+{
+    // Path to slider distributions folder
     string path = "B:\\Projects\\osu-sim\\sliders";
 
-    // Create an output file for slider lengths
+    // Create an output file for slider lengths and velocities
     ofstream slider_lengths_file;
     slider_lengths_file.open("slider_length.txt");
-
-    // Create an output file for slider velocities
     ofstream slider_velocities_file;
     slider_velocities_file.open("slider_velocity.txt");
 
-    // Get the number of total files in the directory
+    // Get the number of files in the directory
     int num_files = 0;
-    for (const auto & entry : filesystem::directory_iterator(path)) {
+    for (const auto &entry : filesystem::directory_iterator(path))
+    {
         num_files++;
     }
+
+    // Parse each file in the sliders folder
     int i = 0;
-    for (const auto & entry : filesystem::directory_iterator(path)) {
-        // Print the progress.
-        printf("Progress: %d/%d: ", i+1, num_files);
+    for (const auto &entry : filesystem::directory_iterator(path))
+    {
+        // Display the progress
+        printf("Progress: %d/%d: ", i + 1, num_files);
+
+        // Open the file
         ifstream file(entry.path());
-        if (file.is_open()) {
-            // Print the file name
+        if (file.is_open())
+        {
+            // Print the filename as part of the progress
             cout << entry.path().filename() << endl;
-            // For each line in the file, split the line by commas and print the first element
+
+            // Get each line of the file (each line is a slider object)
             string line;
-            while (getline(file, line)) {
-                // If the line is empty, skip it
+            while (getline(file, line))
+            {
+                // Skip empty lines
                 if (line.empty())
                     continue;
+
                 // Split the line by commas
                 vector<string> split_line;
                 stringstream ss(line);
-                while (ss.good()) {
+                while (ss.good())
+                {
                     string substr;
                     getline(ss, substr, ',');
                     split_line.push_back(substr);
                 }
-                // If split line does not have 2 elements, skip it
+
+                // Skip the line with the slider-to-circle ratio
                 if (split_line.size() != 2)
                     continue;
+                    
                 // Write the first element to slider_lengths.txt
                 slider_lengths_file << split_line[0] << "\n";
                 // Write the second element to slider_velocities.txt
                 slider_velocities_file << split_line[1] << "\n";
             }
             file.close();
-        } else {
+        }
+        // Error if the file cannot be opened
+        else
+        {
             printf("Unable to open file\n");
         }
         i++;
     }
+
+    // Close the output files and return
     slider_lengths_file.close();
     slider_velocities_file.close();
     return 0;
